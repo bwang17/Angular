7710760c0559865f90cab159c132ed2138e197f0
Derek <benson_wang@protonmail.com>
2017-07-20 21:19:13 -0700

    First Commit

---
+++ github-score/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { HttpService } from './http.service';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  username = null;
+  user = null;
+  score = null;
+  userExist = null;
+  submited = false;
+  constructor(private _httpService: HttpService){}
+
+  getGitHubUser(){
+    this.submited = true;
+    this._httpService.retriveGithubUser(this.username)
+      .then( (user) => {
+        this.userExist = true;
+        this.score = user.followers + user.public_repos
+      })
+        .catch( err => {
+          this.userExist = false;
+      })
+
+  }
+}
